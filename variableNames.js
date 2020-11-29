e14e5fa832cc427da3290309f8bb1fcc359b3047
Max Donchenko <maxx.donchenko@gmail.com>
2020-11-29 18:56:20 +0200

    variable names

---
+++ variableNames.js
@@ -0,0 +1,19 @@
+function youProbablyCantImagineSuchVariableNames() {
+  const $ = 28.51;
+  const _ = 'lo-dash';
+  const plainCharactersButTooLongVariableName = ['yeah', 'it\'s possible'];
+  const кiт = 'ukrainian cat 🐱';
+  const बिल्ली = 'indian cat 🐱';
+  const withDigits123 = 123;
+
+  console.log({
+    $,
+    _,
+    plainCharactersButTooLongVariableName,
+    кiт,
+    बिल्ली,
+    withDigits123,
+  });
+}
+
+exampleFormatter(youProbablyCantImagineSuchVariableNames);
